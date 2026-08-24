bda0c118738ae530d80f2b9e8c6248cc1e88e589
David Valle <davidvalle421@gmail.com>
2026-08-24 07:20:23 -0400

    adding new file to module 1

---
+++ module1/1.5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int length; // this declares a variable
+	length = 7; //this assigns 7 to lenght
+	
+	cout << "The length is ";
+	cout << length; // this displays 7
+	return 0;
+}

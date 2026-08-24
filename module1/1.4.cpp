29aebeec7b916399f7415db51b36ba622011cf28
David Valle <davidvalle421@gmail.com>
2026-08-24 07:14:58 -0400

    added directories for C++ a Bigenners guide

---
+++ module1/1.4.cpp
@@ -0,0 +1,14 @@
+/*
+ * This is a simple C++ proram
+ * Call this file sample.cpp
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "C++ is a power programming.";
+
+	return 0;
+}
+

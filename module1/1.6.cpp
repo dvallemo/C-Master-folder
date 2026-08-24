5468267acbaa592c9f1e21b53318002b94f76d08
David Valle <davidvalle421@gmail.com>
2026-08-24 07:34:53 -0400

    added 1.6 code to module 1

---
+++ module1/1.6.cpp
@@ -0,0 +1,24 @@
+//Using an Operator
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int length; // this declares a variable
+	int width; // this declares another variable
+	int area; // this does too
+
+	length = 7; // this assigns 7 to length
+	width = 5; // this assings 5 to width
+
+	area = length * width; //computes area
+
+	cout << "The area is ";
+	cout << area; // displasy 35
+
+	return 0;
+}
+
+
+
